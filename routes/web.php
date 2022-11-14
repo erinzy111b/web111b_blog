93da5325dc5f1194bca62a791413a4a53a5025cc
wyz <viento2190@gmail.com>
2022-11-14 16:23:48 +0800

    簡化群組

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*未簡化為群組
 Route::get('/hello', 'App\Http\Controllers\SiteController@hello');
-
 Route::get('/users/{id?}', 'App\Http\Controllers\UserController@show');
-
 Route::get('/admin', 'App\Http\Controllers\SiteController@scroll');
-
 Route::get('/posts/{post}/comments/{comment}', function ($post, $comment) {
-    return "posts $post , comments $comment";
-
+return "posts $post , comments $comment";
 //Route::get('/user/{id}', 'App\Http\Controllers\UserController@show');
+});*/
 
+Route::namespace ('App\Http\Controllers')->group(function () {
+    Route::get('/hello', 'SiteController@hello');
+    Route::get('/users/{id?}', 'UserController@show');
+    Route::get('/admin', 'SiteController@scroll');
 });
